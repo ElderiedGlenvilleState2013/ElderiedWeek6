8638e7e5caba4f71408cf96db045483e8189f08b
Elderied McKinney <mckinneydemetrice@icloud.com>
2019-10-10 00:11:39 -0500

    logstash imported and complete

--- pokemonWeek6.py
+++ pokemonWeek6.py
@@ -7,6 +7,7 @@ import logstash
 #logging.basicConfig(filename='python-application-log.log', level=logging.DEBUG, format='%(asctime)s: %(levelname)s: %(message)s')
 test_logger = logging.getLogger('python-logstash-logger')
 test_logger.setLevel(logging.INFO)
+test_logger.setLevel(logging.WARNING)
 test_logger.addHandler(logstash.LogstashHandler('54.91.193.4', 5959, version=1))
 
 class Pokemon:
@@ -50,14 +51,25 @@ class FirePokemon(Pokemon):
 pikachu = ElectricPokemon("Pikachu")
 charmander = FirePokemon('Charmander')
 
-test_logger.debug("{} I choose you".format(pikachu.name))
+test_logger.info("{} I choose you".format(pikachu.name))
 
 test_logger.warning("{0} use {1} attack ".format(pikachu.name, pikachu.attack))
 
 
 test_logger.info("{} I choose you".format(charmander.name))
 
-#logging.debug("{0} use {1} attack ".format(charmander.name, charmander.attack))
-#logging.debug("{0} health is {1}%".format(pikachu.name, pikachu.health))
+test_logger.info("{0} use {1} attack ".format(charmander.name, charmander.attack, charmander.health))
+test_logger.debug("{0} health is {1}%".format(pikachu.name, pikachu.health))
 
+extra = {
+    'test_string': 'python version: ' + repr(sys.version_info),
+    'test_boolean': True,
+    'test_dict': {'a': 1, 'b': 'c'},
+    'test_float': 1.23,
+    'test_integer': 126,
+    'test_list': [1,2,3],
+    'pokemon_name': '{}'.format(pikachu.name),
+    'pikachu_power_type': '{}'.format(pikachu.powerType),
 
+}
+test_logger.info('python-logstash: test extra fields', extra=extra)
